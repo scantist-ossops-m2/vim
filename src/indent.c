7d64c3f8984c7217f920f5b9c22fb8b2fbb3ff9c
TL-Yao <tongle@scantist.com>
2024-04-13 20:05:20 -0700

    commit patch 14250231

--- src/indent.c
+++ src/indent.c
@@ -2045,8 +2045,11 @@ get_lisp_indent(void)
 		    amount += 2;
 		else
 		{
-		    that++;
-		    amount++;
+		    if (*that != NUL)
+		    {
+			that++;
+			amount++;
+		    }
 		    firsttry = amount;
 
 		    while (VIM_ISWHITE(*that))
