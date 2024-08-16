b62a61dd77b7b77f75957160776f5e66a926557c
TL-Yao <tongle@scntist.com>
2024-08-16 06:19:47 +0000

    commit patch 13934918

--- src/ex_getln.c
+++ src/ex_getln.c
@@ -800,6 +800,7 @@ getcmdline_int(
     int		indent,		// indent for inside conditionals
     int		init_ccline)	// clear ccline first
 {
+    static int	depth = 0;	    // call depth
     int		c;
     int		i;
     int		j;
@@ -828,6 +829,9 @@ getcmdline_int(
     cmdline_info_T save_ccline;
     int		did_save_ccline = FALSE;
     int		cmdline_type;
+
+    // one recursion level deeper
+    ++depth;
 
     if (ccline.cmdbuff != NULL)
     {
@@ -880,6 +884,13 @@ getcmdline_int(
 	ccline.cmdpos = indent;
 	ccline.cmdspos = indent;
 	ccline.cmdlen = indent;
+    }
+
+    if (depth == 50)
+    {
+	// Somehow got into a loop recursively calling getcmdline(), bail out.
+	emsg(_(e_command_too_recursive));
+	goto theend;
     }
 
     ExpandInit(&xpc);
@@ -2431,6 +2442,7 @@ theend:
     {
 	char_u *p = ccline.cmdbuff;
 
+	--depth;
 	if (did_save_ccline)
 	    restore_cmdline(&save_ccline);
 	else
