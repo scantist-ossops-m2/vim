2696c9fc6735e7801f7d467dd3684e2a1d24d7b4
TL-Yao <tongle@scntist.com>
2024-08-17 08:07:04 +0000

    commit patch 12761257

--- src/optionstr.c
+++ src/optionstr.c
@@ -2216,7 +2216,7 @@ ambw_end:
 	    if (errmsg == NULL)
 	    {
 		int *oldarray = curbuf->b_p_vsts_array;
-		if (tabstop_set(*varp, &(curbuf->b_p_vsts_array)))
+		if (tabstop_set(*varp, &(curbuf->b_p_vsts_array)) == OK)
 		{
 		    if (oldarray)
 			vim_free(oldarray);
@@ -2255,7 +2255,7 @@ ambw_end:
 	    {
 		int *oldarray = curbuf->b_p_vts_array;
 
-		if (tabstop_set(*varp, &(curbuf->b_p_vts_array)))
+		if (tabstop_set(*varp, &(curbuf->b_p_vts_array)) == OK)
 		{
 		    vim_free(oldarray);
 #ifdef FEAT_FOLDING
