c02a2d3500b9d7e4c05fdd96cbf6bec1f55bfa81
turly221 <feifan@scntist.com>
2024-11-21 03:33:16 +0000

    commit patch 20963208

--- src/register.c
+++ src/register.c
@@ -1238,6 +1238,8 @@ op_yank(oparg_T *oap, int deleting, int mess)
 				// double-count it.
 				bd.startspaces = (ce - cs + 1)
 							  - oap->start.coladd;
+				if (bd.startspaces < 0)
+				    bd.startspaces = 0;
 				startcol++;
 			    }
 			}
