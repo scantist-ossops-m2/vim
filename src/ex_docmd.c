a531e31400914ce61f1d75a3c90288bf5b63a2cf
turly221 <feifan@scntist.com>
2024-12-06 19:06:27 +0000

    commit patch 20572533

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -3915,7 +3915,7 @@ f_fullcommand(typval_T *argvars, typval_T *rettv)
     if (in_vim9script() && check_for_string_arg(argvars, 0) == FAIL)
 	return;
 
-    name = argvars[0].vval.v_string;
+    name = tv_get_string(&argvars[0]);
     if (name == NULL)
 	return;
 
